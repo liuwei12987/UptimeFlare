ef097a89bfa7bc68fd89ce52dd087409e425fc9b
liuwei12987 <60871029+liuwei12987@users.noreply.github.com>
2026-08-25 19:25:11 +0800

    修正导出格式

--- uptime.config.ts
+++ uptime.config.ts
@@ -11,14 +11,16 @@ const pageConfig: PageConfig = {
 
 const workerConfig: WorkerConfig = {
   monitors: [
-    { id: 'fnos_panel', name: '飞牛面板(CF隧道)', method: 'GET', target: 'https://cf.panglina.ggff.net/', statusPageLink: 'https://cf.panglina.ggff.net/' },
+    { id: 'fnos_panel', name: '飞牛面板(CF隧道)', method: 'GET', target: 'https://cf.panglina.ggff.net/' },
     { id: 'wenjian_cccd', name: '文件存储站(cc.cd)', method: 'GET', target: 'https://wenjian.panglina.cc.cd/' },
     { id: 'wenjian_ccwu', name: '文件存储站(ccwu.cc)', method: 'GET', target: 'https://wenjian.panglina.ccwu.cc/' },
     { id: 'wenjian_ggff', name: '文件存储站(ggff.net)', method: 'GET', target: 'https://wenjian.panglina.ggff.net/' },
     { id: 'wechat_bridge', name: '微信桥接', method: 'GET', target: 'https://fn.panglina.ggff.net/', expectedCodes: [200, 401] },
     { id: 'docker_mirror', name: 'Docker镜像代理', method: 'GET', target: 'https://docker.panglina.ggff.net/v2/', expectedCodes: [200, 401] },
-    { id: 'gh_proxy', name: 'GitHub加速', method: 'GET', target: 'https://gh.panglina.ggff.net/', expectedCodes: [200] },
+    { id: 'gh_proxy', name: 'GitHub加速', method: 'GET', target: 'https://gh.panglina.ggff.net/' },
   ],
 }
 
-export default { pageConfig, workerConfig }
+const maintenances: MaintenanceConfig[] = []
+
+export { maintenances, pageConfig, workerConfig }
